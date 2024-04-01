c961bcd1f3ab3047d86dc74b552dfbaa6a59eba2
Nathaniel Colon <nathanielcolon12@gmail.com>
2024-04-01 14:14:17 -0400

    Added routing for html pages

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const http = require("http");
+const fs = require("fs");
+const url = require("url");
 
 // Create a local server to receive data from
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "Hello World!",
-    })
-  );
+  q = url.parse(req.url, true);
+  console.log(req.url);
+  let filename = "." + req.url + ".html";
+  fs.readFile(filename, function (err, data) {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    return res.end();
+  });
 });
 
 server.listen(8000);
